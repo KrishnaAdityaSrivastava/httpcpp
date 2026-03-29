149fc406e5cd3c488992ecc33621c2e17f08daf0
Krishna Aditya Srivastava <69421609+KrishnaAdityaSrivastava@users.noreply.github.com>
2026-03-29 12:54:17 +0530

    Refactor socket classes to fix build and initialization flow

--- Sockets/listenSocket.cpp
+++ Sockets/listenSocket.cpp
@@ -1,20 +1,13 @@
 #include "listenSocket.hpp"
 
-HTTP::ListenSocket::ListenSocket(int domain, int service,int protocol, int port, u_long interface,int bklg) : BindSocket(domain,service,protocol,port,interface){
-    backlog = bklg;
+HTTP::ListenSocket::ListenSocket(int domain, int service, int protocol, int port, u_long interface, int bklg)
+    : BindSocket(domain, service, protocol, port, interface), backlog(bklg), listening(-1) {
     start_listening();
     test_connection(listening);
 }
 
-void HTTP::ListenSocket::start_listening(){
-    listening = listen(get_sock(),backlog);
-}
+void HTTP::ListenSocket::start_listening() { listening = listen(get_sock(), backlog); }
 
-int HTTP::ListenSocket::get_listening(){
-    return listening;
-}
+int HTTP::ListenSocket::get_listening() const { return listening; }
 
-int HTTP::ListenSocket::get_backlog(){
-    return backlog;
-}
-
+int HTTP::ListenSocket::get_backlog() const { return backlog; }
--- Sockets/socket.hpp
+++ Sockets/socket.hpp
@@ -1,27 +1,32 @@
 #ifndef Socket_h
 #define Socket_h
 
-#include <stdio.h>
-#include <iostream>
-#include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
 
-namespace HTTP
-{
-    class Socket{
-        private:
-            struct sockaddr_in address;
-            int sock;
+namespace HTTP {
+class Socket {
+  private:
+    struct sockaddr_in address{};
+    int sock{-1};
+    int connection{-1};
 
-        public:
-            Socket( int domain, int service,int protocol, int port, u_long interface);
-            virtual int connect_network(int sock, struct sockaddr_in address) = 0;
-            void test_connection(int);
+  protected:
+    void establish_connection();
 
-            struct sockaddr_in get_address();
-            int get_sock();
+  public:
+    Socket(int domain, int service, int protocol, int port, u_long interface);
+    virtual ~Socket() = default;
 
-            void set_connection(int domain);
-    };
-}
+    virtual int connect_network(int sock, struct sockaddr_in address) = 0;
+    void test_connection(int test_obj) const;
+
+    struct sockaddr_in get_address() const;
+    int get_sock() const;
+    int get_connection() const;
+
+    void set_connection(int con);
+};
+} // namespace HTTP
+
 #endif
