3e2eac268f305663df646b102b9a760f342b4955
Krishna Aditya Srivastava <69421609+KrishnaAdityaSrivastava@users.noreply.github.com>
2026-04-14 21:24:13 +0530

    Simplify routing to plain function handlers

--- Server/server.hpp
+++ Server/server.hpp
@@ -1,17 +1,13 @@
 #ifndef Server_h
 #define Server_h
 
-#include <functional>
 #include <string>
 #include <vector>
-
-#include "thread_pool.hpp"
 
 #include "request.hpp"
 #include "response.hpp"
 #include "route.hpp"
 
-#include "../Cache/cache.hpp"
 #include "../network.hpp"
 
 namespace HTTP {
@@ -20,27 +16,17 @@ class Server {
     ListenSocket* socket;
 
     int accepter();
-    //void process_request(int client_socket, Request& req);
-    
-    HTTP::ThreadPool thread_pool;
-
-    std::function<std::string(Request)> custom_handler;
     std::vector<Route> routes;
-    HTTP::Cache cache;
 
   public:
     Server(int domain, int service, int protocol, int port, u_long interface, int bklg);
+    ~Server();
 
     ListenSocket* get_socket();
     void handle_client_connection(int client_socket);
 
-    //void set_handler(std::function<std::string(Request)> operation);
-
-    void get(const std::string& path, std::function<HTTP::Response(const Request&)> handler);
-    void post(const std::string& path, std::function<HTTP::Response(const Request&)> handler);
-
-    std::string make_key(const std::string& method, const std::string& path);
-    Cache& get_cache();
+    void get(const std::string& path, HandlerFn handler);
+    void post(const std::string& path, HandlerFn handler);
 
     void launch();
 };
